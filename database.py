bb16ce5de4fa079d4992f873b477d8b207f2dc19
Aadio802 <164066771+Aadio802@users.noreply.github.com>
2026-01-13 23:34:02 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,12 +1,21 @@
 import aiosqlite
 
 async def init_db():
-    async with aiosqlite.connect("users.db") as db:
+    async with aiosqlite.connect("botdata.db") as db:
         await db.execute("""
-        CREATE TABLE IF NOT EXISTS users (
-            user_id INTEGER PRIMARY KEY,
-            rating_sum INTEGER DEFAULT 0,
-            rating_count INTEGER DEFAULT 0
-        )
+            CREATE TABLE IF NOT EXISTS users (
+                user_id INTEGER PRIMARY KEY,
+                rating_sum INTEGER DEFAULT 0,
+                rating_count INTEGER DEFAULT 0,
+                is_premium INTEGER DEFAULT 0,
+                is_banned INTEGER DEFAULT 0
+            );
+        """)
+        await db.execute("""
+            CREATE TABLE IF NOT EXISTS reports (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                reporter INTEGER,
+                reported INTEGER
+            );
         """)
         await db.commit()
